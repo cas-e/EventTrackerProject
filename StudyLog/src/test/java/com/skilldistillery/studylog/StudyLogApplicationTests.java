efb11b980324e6660c80cad32eb4992c0adc59d4
cas-e <caseyk6.28@gmail.com>
2023-10-06 15:56:25 -0700

    create spring boot rest project, add jpa deps

---
+++ StudyLog/src/test/java/com/skilldistillery/studylog/StudyLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.studylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
